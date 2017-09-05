e55dd3a5b33f7167026815e66e08017adaa13489
Stefano Corra <stefano.corra@hotmail.com>
2017-09-05 18:33:10 -0400

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require ('express');
+const express = require('express');
+const routes = require('./routes/api');
 
 // set up express app
 const app = express();
 
-app.get('/api', function(req, res){
-  console.log('GET request');
-  res.end();
-});
+// initialize routes
+app.use('/api',routes);
+
+// app.get('/api', function(req, res){
+//   console.log('GET request');
+//   res.send({name:'Yoshi'});
+// });
 
 //listen for requests
 app.listen(process.env.port || 4000,function(){
