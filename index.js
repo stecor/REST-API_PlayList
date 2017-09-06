1c935ddb7420a1167f132885fb6ad0bc331f074a
Stefano Corra <stefano.corra@hotmail.com>
2017-09-05 21:07:58 -0400

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes/api');
+const bodyParser = require('body-parser');
+
 
 // set up express app
 const app = express();
+
+app.use(bodyParser.json());
 
 // initialize routes
 app.use('/api',routes);
