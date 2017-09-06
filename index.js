d2c08c13e8fb489cfe43a598051646ee010c3def
Stefano Corra <stefano.corra@hotmail.com>
2017-09-05 21:42:51 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const routes = require('./routes/api');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago', { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
