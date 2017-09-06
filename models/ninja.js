b0b5e420e74db6fbe76827eca8530b48a4ebb916
Stefano Corra <stefano.corra@hotmail.com>
2017-09-06 00:12:10 -0400

    seventh commit

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// create geolocation Schema
+const GeoSchema = new Schema({
+  type:{
+    type: String,
+    default: "Point"
+  },
+  coordinates:{
+    type:[Number],
+    index: "2dsphere"
+  }
+});
+
 //create ninja Schema & model
 const NinjaSchema = new Schema({
   name: {
@@ -13,8 +25,9 @@ const NinjaSchema = new Schema({
   available:{
     type: Boolean,
     default: false
-  }
-  //add in geo location
+  },
+  //add in geo location Schema
+  geometry: GeoSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
